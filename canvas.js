415154f5fa8a468ff6f9a4b31189c72ec9fde0bc
Yusef Jones <94017905+Yuey93@users.noreply.github.com>
2022-04-29 01:39:59 +0100

    Update canvas.js

--- canvas.js
+++ canvas.js
@@ -5,40 +5,6 @@ canvas.height = window.innerHeight;
 
 let c = canvas.getContext('2d');
 
-// Rectangles
-// c.fillStyle = 'rgba(255, 100, 0, 0.5)';
-// c.fillRect(100, 100, 100, 100);
-// c.fillStyle = 'rgba(0, 100, 0, 0.5)';
-// c.fillRect(50, 500, 100, 100);
-// c.fillStyle = 'rgba(0, 0, 200, 0.5)';
-// c.fillRect(150, 300, 100, 100);
-// console.log(canvas);
-
-// Lines
-// c.beginPath();
-// c.moveTo(50, 300);
-// c.lineTo(150, 100);
-// c.lineTo(250, 300);
-// c.strokeStyle = '#fa34a3';
-// c.lineWidth = 2;
-// c.stroke();
-
-// Arcs
-// c.beginPath();
-// c.arc(300, 300, 30, 0, Math.PI*2, false);
-// c.strokeStyle = 'blue';
-// c.stroke();
-
-// for (let i = 0; i < 3; i++) {
-//     let x = Math.random() * window.innerWidth;              // Math.random() returns random value between 0 and 1
-//     let y = Math.random() * window.innerHeight;
-
-// c.beginPath();
-// c.arc(x, y, 30, 0, Math.PI*2, false);
-// c.strokeStyle = 'blue';
-// c.stroke();
-// }
-
 let mouse = {
     x: undefined,
     y: undefined
